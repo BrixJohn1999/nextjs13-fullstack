b5ea6307a2676640d827d02d596797c87344f3be
Brix John Novesteras <bnovesteras@jairosolution.com>
2023-06-06 16:13:28 +0800

    page layout

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bloglayout = ({children}) => {
+  return (
+    <div>
+<h1>HI this is blog</h1>
+    {children}</div>
+  )
+}
+
+export default Bloglayout
--- src/app/home/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HOme() {
-  return (
-    <div>HOme</div>
-  )
-}
-
-export default HOme
