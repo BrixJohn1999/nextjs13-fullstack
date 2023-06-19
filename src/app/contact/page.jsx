833ceb455eb0e3b0ddd0ff988a8e29ac3fd6188c
Brix John Novesteras <brixjohnovesteras@gmail.com>
2023-06-19 08:58:47 +0800

    contact page and animation for image

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "../../components/Button/Button";
 function Contact() {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            width={500}
+            height={500}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="text" placeholder="Email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="Message"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
